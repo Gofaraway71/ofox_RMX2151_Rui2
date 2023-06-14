dae176439d2a3ff4954a40af5f679408dad58592
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2023-06-14 21:37:31 +0100

    Delete recovery/root directory

--- recovery/root/system/bin/init.device.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-prjVersion=$(cat /proc/oplusVersion/prjVersion)
-if [ "$prjVersion" = 20682 ]; then
-  resetprop ro.build.product RMX2151
-  resetprop ro.product.device RMX2151
-  resetprop ro.product.model RMX2151
-fi
